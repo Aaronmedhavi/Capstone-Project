c3230706d39fdf87cb2ad22e4e2edd27dec4f1e5
Gramone <154248035+Gramonesk@users.noreply.github.com>
2024-12-02 13:37:59 +0700

    + EffectParticle

--- Assets/Script/Lightning.cs
+++ Assets/Script/Lightning.cs
@@ -1,12 +1,22 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 public class Lightning : ProjectileParticle
 {
+    [Header("Lightning Settings")]
+    [SerializeField] private float chain;
+    [SerializeField] private float distance;
+    [SerializeField] private GameObject effectObject;
+
+    public List<GameObject> hits = new(); 
     public override void ParticleLogic(GameObject other)
     {
         Debug.Log("a");
         if(other.TryGetComponent<IEntity>(out var hit))
         {
             hit.OnReceiveDamage(Damage, EnemyInvisDuration);
+            var obj = ObjectPoolManager.GetObject(effectObject, false, ObjectPoolManager.PooledInfo.Particle);
+            obj.transform.position = other.transform.position;
+            obj.gameObject.SetActive(true);
         }
         Stop();
     }
